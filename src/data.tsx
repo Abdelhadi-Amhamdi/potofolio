bce611fe738c45958daea14217e0c303302ea9e9
Abdelhadi-Amhamdi <aamhamdi943@gmail.com>
2024-09-21 18:12:07 +0100

    [add] social media links

--- src/data.tsx
+++ src/data.tsx
@@ -1,4 +1,4 @@
-import {FaReact, FaPython, FaDocker, FaWordpress} from 'react-icons/fa'
+import {FaReact, FaPython, FaDocker, FaWordpress, FaInstagram, FaLinkedin, FaTwitter, FaDev, FaGithub} from 'react-icons/fa'
 import {SiNginx, SiMariadb, SiCplusplus, SiC} from 'react-icons/si'
 
 export const projects = [
@@ -6,36 +6,42 @@ export const projects = [
         title : "Ft-trancendence",
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "web",
+        img : "/pong.jpg",
         tech : [<FaReact />, <FaPython />],
     },
     {
         title : "Inception",
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "devops",
+        img : "/img.png",
         tech : [<FaDocker />, <SiNginx/>, <SiMariadb /> , <FaWordpress />],
     },
     {
         title : "Ft-IRC",
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "unix",
+        img : "/irc.jpg",
         tech : [<SiCplusplus />],
     },
     {
         title : "Cube-3D",
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "graphic",
+        img : "/cube.jpg",
         tech : [<SiC />],
     },
     {
         title : "Minishell",
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "unix",
+        img : "/minishell.jpg",
         tech : [<SiC />],
     },
     {
         title : "Philosophers",
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "unix",
+        img : "/img.png",
         tech : [<SiC />],
     },
 ]
@@ -43,17 +49,39 @@ export const projects = [
 
 export const infos = {
     address : "Khouribga",
-    code : "25000",
+    code : "25000, massira",
     phone : "+2126 90 73 14 52",
     email : "aamhamdi943@gmail.com",
 }
 
+export const links  = [
+    {
+        icon : <FaInstagram />,
+        link : "https://www.instagram.com/__ab__am/"
+    },
+    {
+        icon : <FaLinkedin />,
+        link : "https://www.linkedin.com/in/abdelhadi-amhamdi-a680a5209/"
+    },
+    {
+        icon : <FaTwitter />,
+        link : "https://x.com/AbdelhadiAmham1"
+    },
+    {
+        icon : <FaDev />,
+        link : "https://dev.to/aamhamdi"
+    },
+    {
+        icon : <FaGithub />,
+        link : "https://github.com/Abdelhadi-Amhamdi"
+    },
+]
 
 export const articles = [
     {
         title : "What are CPU registers",
         desc : "Computer registers are small, high-speed storage units within a computer's central processing unit (CPU) used to temporarily hold data and instructions",
-        image : "https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fphckyu82930s0qyx5o4a.jpg",
+        image : "",
         link : "https://dev.to/aamhamdi/what-are-cpu-registers-4275",
         tags : ["unix"]
     }
