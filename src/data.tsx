5be065cca0f0a962a912ec2c78b1143f0204f07e
Abdelhadi-Amhamdi <aamhamdi943@gmail.com>
2024-09-22 14:38:45 +0100

    [update] data

--- src/data.tsx
+++ src/data.tsx
@@ -1,5 +1,5 @@
 import {FaReact, FaPython, FaDocker, FaWordpress, FaInstagram, FaLinkedin, FaTwitter, FaDev, FaGithub} from 'react-icons/fa'
-import {SiNginx, SiMariadb, SiCplusplus, SiC} from 'react-icons/si'
+import {SiNginx, SiMariadb, SiCplusplus, SiC, SiPostgresql, SiTailwindcss} from 'react-icons/si'
 
 export const projects = [
     {
@@ -7,13 +7,13 @@ export const projects = [
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "web",
         img : "/pong.jpg",
-        tech : [<FaReact />, <FaPython />],
+        tech : [<FaReact />, <FaPython />, <SiPostgresql />, <SiTailwindcss />],
     },
     {
         title : "Inception",
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "devops",
-        img : "/img.png",
+        img : "/inc.jpg",
         tech : [<FaDocker />, <SiNginx/>, <SiMariadb /> , <FaWordpress />],
     },
     {
@@ -41,6 +41,20 @@ export const projects = [
         title : "Philosophers",
         description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
         categorie : "unix",
+        img : "/philo.jpg",
+        tech : [<SiC />],
+    },
+    {
+        title : "push swap",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
+        categorie : "algorithm",
+        img : "/img.png",
+        tech : [<SiC />],
+    },
+    {
+        title : "so long",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, aspernatur!",
+        categorie : "graphic",
         img : "/img.png",
         tech : [<SiC />],
     },
@@ -81,7 +95,7 @@ export const articles = [
     {
         title : "What are CPU registers",
         desc : "Computer registers are small, high-speed storage units within a computer's central processing unit (CPU) used to temporarily hold data and instructions",
-        image : "",
+        image : "/cpu.jpg",
         link : "https://dev.to/aamhamdi/what-are-cpu-registers-4275",
         tags : ["unix"]
     }
